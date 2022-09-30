7a6fdfb53127675597716495cb02ccc9629e29ae
Edwin Fernandez <fernandez.edwin86@protonmail.com>
2022-09-30 10:44:54 -0400

    Created tweets model. Set up form to schedule tweets.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@
 # password_confirmation:string virtual
 
 class User < ApplicationRecord
-  has_many :twitter_accounts 
+  has_many :twitter_accounts
+  has_many :tweets
+  
   has_secure_password
 
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a vaild email address" }
